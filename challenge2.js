49851e19508d9aa6e430d4c3a60aed890376c0ca
Isaac <bennisaac24@gmail.com>
2024-12-01 23:38:54 +0300

    added a condition to suspende the license of demerits exceed 12, inside of an another if statement to check the demerits

--- challenge2.js
+++ challenge2.js
@@ -6,12 +6,16 @@ function checkspeed(){
     //if condition to take merits
     if (speed <= limit){
         console.log("OK")
+    
+    } else if (speed > 70){
+        let demeritpoints = Math.ceil((speed - limit)/5)
+        console.log(`You have ${demeritpoints}`)
 
-        if (speed > 70){
-            let demeritpoints = Math.ceil(())
-
-                if merit
-        }
+            if (demeritpoints > 12){
+                    console.log("License suspended")
+            }
     }
+    
 }
 
+checkspeed();

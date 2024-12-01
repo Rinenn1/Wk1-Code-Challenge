ce39c475aa8066332a762206efdd2e412a26b24a
Isaac <bennisaac24@gmail.com>
2024-12-01 23:50:42 +0300

    added an input for both basic salary and benefits in variables each; basicSalary and selfBenefits respectively

--- challenge3.js
+++ challenge3.js
@@ -1,3 +1,6 @@
 function checkNetSalary(){
-    
+    let basicSalary = Number(prompt("What is your basic Salary?: "));
+    let selfBenefits = Number(prompt("What is your Contribution Benefit?: "));
+
+
 }

b7afee0a14927ed2d4532e55dad90b467452b042
Isaac <bennisaac24@gmail.com>
2024-12-01 23:06:06 +0300

    added code to display grade for score above 79 as 'A'

--- challenge1.js
+++ challenge1.js
@@ -4,6 +4,8 @@ function grades() {
     if (score < 0 || score > 100) {
         console.log("Invalid input! Please enter a number between 0 and 100.");
         return;
+    } else if (score > 79) {
+        console.log("Your Grade is A");
     } 
     
 }

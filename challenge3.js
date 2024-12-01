6db6382fd89d2a02af8356fbf334f52eb20503bf
Isaac <bennisaac24@gmail.com>
2024-12-01 23:51:56 +0300

    changed the prompt for basic salary to ask for monthly salary

--- challenge3.js
+++ challenge3.js
@@ -1,5 +1,5 @@
 function checkNetSalary(){
-    let basicSalary = Number(prompt("What is your basic Salary?: "));
+    let basicSalary = Number(prompt("What is your basic Monthly Salary?: "));
     let selfBenefits = Number(prompt("What is your Contribution Benefit?: "));
 
 

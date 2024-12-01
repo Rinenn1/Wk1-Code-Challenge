e3e4aac123c0dcadb27a73951d60655f87a1c856
Isaac <bennisaac24@gmail.com>
2024-12-01 23:19:17 +0300

    added a variable called 'merit' that adds one to itself

--- challenge2.js
+++ challenge2.js
@@ -1,7 +1,9 @@
 //input of speed of car
 const speed = Number(prompt("What speed are you going at?: "));
 
+const merit = +1;
+
 //if condition to take merits
 if (speed <= 70){
     console.log("OK")
-}
+} else if ()

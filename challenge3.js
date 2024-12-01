07d59e6239067bb653ee4061a469463ac8613254
Isaac <bennisaac24@gmail.com>
2024-12-01 23:54:51 +0300

    added a variable to calculate gross yearly salary from basic salary

--- challenge3.js
+++ challenge3.js
@@ -2,5 +2,8 @@ function checkNetSalary(){
     let basicSalary = Number(prompt("What is your basic Monthly Salary?: "));
     let selfBenefits = Number(prompt("What is your Contribution Benefit?: "));
 
+    let grossSalary = basicSalary * 12;
+
+    
 
 }

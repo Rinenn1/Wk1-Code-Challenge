f080d49905624684e464a80d4d255a092fbf807b
Isaac <bennisaac24@gmail.com>
2024-12-03 13:33:05 +0300

    working code

--- challenge1.js
+++ challenge1.js
@@ -1,21 +1,33 @@
-function grades() {
-    let score = Number(prompt("What is your score?: "));
+//Import the readline from node.
+import readline from 'readline';
 
-    if (score < 0 || score > 100) {
-        console.log("Invalid input! Please enter a number between 0 and 100.");
-        return;
-    } else if (score > 79) {
-        console.log("Your Grade is A");
-    } else if (score >= 60 && score <= 79) {
-        console.log("Your Grade is B");
-    }  else if (score >= 50 && score <= 59) {
-        console.log("Your Grade is C");
-    } else if (score >= 40 && score <= 49) {
-        console.log("Your Grade is D");
-    } else {
-        console.log("Your Grade is E");
-    }
+//create an interface for the readline
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+//create the grade function
+function grades() {    
+    //parse the user input into a variable
+    rl.question('What is your score?: ',
+        (input) => {
+            let score = parseInt(input);
+
+            //if condition for the grades such that "< 0 - is invalid & > 100 is also invalid"
+            if (score >= 0 && score <= 100){
+                //use ternary operator for the grades
+                score > 79 ? console.log("You Have an A") : score >= 60 ? console.log("You have a B") : score > 49 ? console.log("YOu have a C") : score > 40 ? console.log("You have a D") : console.log("You have an E")
+            } else {
+                console.log("Invalid Input!!")
+            }
+                
+            //close the readline function
+            rl.close();
+
+        });
+    
 }
 
+//call the grade function
 grades();
